2c9f7c5ed443dfacf2ee3219a4ca80c8b29a9244
c0023084 <y.yue1@newcstle.ac.uk>
2021-02-10 19:44:33 +0000

    Update Database

--- server/database.js
+++ server/database.js
@@ -1,20 +1,5 @@
 const mysql = require('mysql');
 
-// Database Connection for Production
-
-// let config = {
-//     user: process.env.SQL_USER,
-//     database: process.env.SQL_DATABASE,
-//     password: process.env.SQL_PASSWORD,
-// }
-
-// if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-// }
-
-// let connection = mysql.createConnection(config);
-
-// Database Connection for Development
 
 let connection = mysql.createConnection({
   host: 'team5.crgo2bd9ywen.eu-west-2.rds.amazonaws.com',
