b8af8e2922c8bb697122b628954e974ea9f91003
Yifan Yue <y.yue1@newcstle.ac.uk>
2021-02-28 15:35:08 +0000

    update paymentAPI.js

--- server/routes/paymentApi.js
+++ server/routes/paymentApi.js
@@ -1,27 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var connection = require('../mysql/database');
+const axios = require('axios');
 
-
+router.get('/api/payment', (req, res) => {
+    return payThroughHorsePay((err, data) => {
+        if (err) return res.send(400);
+        res.end();
+      });
+    });
 module.exports = router;
 
-function queryStoreID(storeID, callback){
-    let sql = `select ID from RestaurantInfo`;
-    connection.query(sql, function(error, results, fields){
-        if (error) {
-            callback(error);
-        } else {
-            if (results.length) {
-				storeID = JSON.stringify(results);
-                callback(null, {
-                    code: 0,
-	                data: storeID
-                });
-            } else {
-                callback(null, {
-                    code: 1
-                });
-            }
-        }
+function payThroughHorsePay(callback){
+    const url = 'http://homepages.cs.ncl.ac.uk/daniel.nesbitt/CSC8019/HorsePay/HorsePay.php';
+
+    return axios.post(url, {
+        //these information should get from frontend
+        'storeID' : 'Team05',
+        'customerID' : 'abc123',
+        'date' : '01/12/2021',
+        'time' : '12:00',
+        'timeZone' : 'GMT',
+        'transactionAmount' : 15.99,
+        'currencyCode' : 'GBP'
     })
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.log(error);
+        return callback(error);
+    });
 }
