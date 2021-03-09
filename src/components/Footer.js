86837e2929d031e94faa364d2602ca18f89b344f
Karim Alsaka <b6017371@newcastle.ac.yk>
2021-03-09 18:49:07 +0000

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Footer.css';
 
-function Footer() {
+function Footer(props) {
   return (
     <div className='footer-container'>
-     <p>Footer</p>
+     <ul>
+       <li>{props.companyInfo.RestaurantName}</li>
+       <li>{props.companyInfo.Slogan}</li>
+       <li>{props.companyInfo.Address} {props.companyInfo.PostCode}</li>
+       <li>{props.companyInfo.Email}</li>
+       <li>{props.companyInfo.Telephone}</li>
+       <li>Weekday Opening Times</li>
+       <li>{props.companyInfo.WeekdayOpeningTime} - {props.companyInfo.WeekdayClosingTime}</li>
+       <li>Weekend Opening Times</li>
+       <li>{props.companyInfo.WeekendClosingTime} - {props.companyInfo.WeekendOpeningTime}</li>
+     </ul>
     </div>
   );
 }
 
 export default Footer;
+
