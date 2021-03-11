cb0a5b1edd8a0bb9b6802dc6749badc6b5f55ebb
c0074261 <c0074261@newcastle.ac.uk>
2021-03-11 01:48:44 +0000

    update the style of order item list and remove hard code

--- src/components/OrderResult.js
+++ src/components/OrderResult.js
@@ -4,31 +4,16 @@ import './OrderResult.css';
 //Now App.js state is in this component, you can access it by doing {this.props.customerOrder.item1} or {this.props.customerDetails.name} as seen in App.js state.
 //You can use these props to populate your forms with real order and customer data.
 
-// providing defaultProps to view the page.
-// maybe remove it later.
 const defaultProps = {
     customerOrder: {
-        orderId: '205-1752345-4325359',
-        orderTime: '17 July 2020',
-        TotalCost: 0.0,
-        Items: [
-            {
-                flavor: 'banana',
-                size: 'Large',
-                cost: '￡3.22',
-                count: 2
-            },
-            {
-                flavor: 'orange',
-                size: 'Small',
-                cost: '￡1.25',
-                count: 3
-            }
-        ]
+        orderId: '',
+        orderTime: '',
+        TotalCost: 0,
+        Items: []
     },
     customerDetails: {
-        deliveryAddress: 'John 115D LONDON 1 BLANDFORD SQUARE XXXXX xxxx United Kingdom',
-        billingAddress: 'John 115D LONDON 1 BLANDFORD SQUARE XXXXX xxxx United Kingdom'
+        deliveryAddress: '',
+        billingAddress: ''
     }
 };
 
@@ -37,14 +22,6 @@ function OrderResult(props) {
 
     const { orderID: orderId, orderTime, TotalCost: totalCost, Items: orderItems } = customerOrder;
     const { deliveryAddress, billingAddress } = customerDetails;
-
-    console.log(orderItems);
-    // compute total cost
-    // const totalCost = Items.reduce((total, item) => {
-    //     total += item.count * +item.cost.replace(/[^\d|.]/g, '');
-
-    //     return +total.toFixed(2);
-    // }, 0);
 
     return (
         <div id="orderResult" className="container-fluid">
@@ -56,7 +33,7 @@ function OrderResult(props) {
             <hr className="col-10" style={{ marginLeft: '0.5rem' }} />
 
             <div className="col-12">
-                <div className="col-12" style={{ color: '#dddddd' }}>
+                <div className="col-12 mb-2" style={{ color: '#dddddd' }}>
                     <div className="row">
                         <div className="col-4">ITEM</div>
                         <div className="col-4">QTY</div>
@@ -66,13 +43,16 @@ function OrderResult(props) {
 
                 {orderItems.map((item, index) => {
                     return (
-                        <div className="col-12" key={index}>
+                        <div className="col-12 mb-2" key={index}>
                             <div className="row" style={{ color: '#333333' }}>
                                 <div className="col-4">
-                                    {item.Flavour}&nbsp;  {item.Size}
+                                    <div>{item.Flavour}&nbsp;</div>
+                                    <div>{item.Size}</div>
                                 </div>
                                 <div className="col-4">{item.Quantity}</div>
-                                <div className="col-4">£{item.TotalCost * item.Quantity}</div>
+                                <div className="col-4">
+                                    £{(item.TotalCost * item.Quantity).toFixed(2)}
+                                </div>
                             </div>
                         </div>
                     );
@@ -86,7 +66,6 @@ function OrderResult(props) {
                     <div className="row">
                         <div className="col-4"></div>
                         <div className="col-4">Subtotal:</div>
-                        {/* TODO: compute subtotal cost */}
                         <div className="col-4">£{totalCost}</div>
                     </div>
                 </div>
@@ -108,8 +87,6 @@ function OrderResult(props) {
                         <div className="col-12">
                             <div className="row">
                                 <div className="col-6" style={{ paddingLeft: 0 }}>
-                                    {/* TODO: remove fake data */}
-                                    John 115D LONDON 1 BLANDFORD SQUARE XXXXX xxxx United Kingdom
                                     {billingAddress}
                                 </div>
                             </div>
@@ -121,8 +98,6 @@ function OrderResult(props) {
                         <div className="col-12">
                             <div className="row">
                                 <div className="col-6" style={{ paddingLeft: 0 }}>
-                                    {/* TODO: remove fake data */}
-                                    John 115D LONDON 1 BLANDFORD SQUARE XXXXX xxxx United Kingdom
                                     {deliveryAddress}
                                 </div>
                             </div>
