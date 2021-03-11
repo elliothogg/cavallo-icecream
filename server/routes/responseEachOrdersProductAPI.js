f85fefe34b340727a8a00cb7f37d2429be02a450
Yifan Yue <y.yue1@newcstle.ac.uk>
2021-03-11 16:55:01 +0000

    separate orders api and eachordersproducts api

--- server/routes/responseEachOrdersProductAPI.js
+++ server/routes/responseEachOrdersProductAPI.js
@@ -4,21 +4,23 @@ var router = express.Router();
 var connection = require('../mysql/database');
 const urlendodedParser= bodyParser.urlencoded({ extended: false });
 
-router.get('/api/responseOrderData', urlendodedParser, function(req, res, next){
+router.get('/api/responseEachOrdersProductsData', urlendodedParser, function(req, res, next){
+    var orderID = req.query.orderID;
+    console.log("OrderID:" + orderID)
 
-    return queryOrdersInfo((err, data) => {
+    return queryEachOrdersProductsInfo(orderID, (err, data) => {
         if (err) return res.send(400);//upstream request failed
         res.setHeader('Content-Type', 'application/json');
 		console.log("get success")
         console.log(data);
         res.send(data);
     })
-})
 
+})
 module.exports = router;
 
-function queryOrdersInfo(callback) {
-    let sql = `SELECT * FROM Orders`;
+function queryEachOrdersProductsInfo(orderID, callback) {
+    let sql = `SELECT * FROM EachOrdersProducts WHERE orderID = '${orderID}'`;
     connection.query(sql, function(error, results, fields){
         if (error) {
             callback(error);
@@ -36,4 +38,3 @@ function queryOrdersInfo(callback) {
         }
     })
 }
-
